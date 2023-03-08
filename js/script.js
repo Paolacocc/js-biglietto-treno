93cd7ed71e4a7a91b29d7ba68b394b11eedefe23
Paolacocc <coccettipaola@gmail.com>
2023-03-08 15:48:45 +0100

    ticket generator done

---
+++ js/script.js
@@ -0,0 +1,40 @@
+//INPUT
+// chiedere il numero dei km
+const kmNumber = parseInt(prompt("Quanti km devi percorrere?"));
+//chiedere l'eta
+const userAge = parseInt(prompt("quanti anni hai?"));
+console.log(kmNumber, userAge);
+//LOGICA
+// calcolare il prezzo del biglietto
+const ticketPrice = kmNumber * 0.21;
+console.log(ticketPrice);
+//applicare uno sconto del 20% per minorenni e del 40% per gli over 65
+
+//discout per i minorenni
+const youngDiscount = ticketPrice * 0.2;
+console.log(youngDiscount);
+const discountUnderAge = (ticketPrice - youngDiscount).toFixed(2);
+console.log(discountUnderAge);
+
+//discount per pensionati
+const elderlyDiscount = ticketPrice * 0.4;
+const discountRetirees = (ticketPrice - elderlyDiscount).toFixed(2);
+console.log(discountRetirees);
+
+
+// applicare la consizione nei diversi casi
+let finalPrice = "";
+
+if (userAge <= 18 ){
+    finalPrice = discountUnderAge;
+} else if (userAge >= 65){
+    finalPrice = discountRetirees;
+} else {
+    finalPrice = ticketPrice
+}
+console.log(finalPrice);
+
+//OUTPUT
+//stampare il prezzo finale del biglietto con massimo due decimali
+
+document.getElementById('ticket').innerHTML = finalPrice;
